ddb0601c1796300563b6b56bc8951056537717b8
gomesrhu <gomes.trabalho2022@gmail.com>
2026-04-23 17:28:04 -0300

    fix:raise RuntimeError ao inves de uma mensagem simples no ex. 08

--- studies/decorators/08_validador_intervalo_decorator.py
+++ studies/decorators/08_validador_intervalo_decorator.py
@@ -3,12 +3,12 @@ def interval(min, max):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             if not (min < result < max):
-                return f"Error. The result ({result}) of the decorated function '{func.__name__}' isn't between {min} and {max}"
-            else:
-                return result
+                error_msg = f"Error.Result ({result}) of the decorated function '{func.__name__}' isn't between {min} and {max}"
+                raise RuntimeError(error_msg)
+            return result
         return wrapper
     return decorator
 
-@interval(2, 10)#min=2 max=10 in this exemple
+@interval(2, 10)
 def decorated_sum(a, b):
     return a+b
